871344edc8722c53ff6dddcc3df19b4c6aaa8af9
Tatiana-Popova <Tatiana-Popova@users.noreply.github.com>
2022-02-03 00:06:22 +0400

    refactor fileDiff.js for Eslint

--- src/fileDiff.js
+++ src/fileDiff.js
@@ -5,12 +5,12 @@ import path from 'path';
 const parse = (filePath, file) => {
   const extention = path.extname(filePath);
   if (extention === '.json') return JSON.parse(file);
+  return null;
 };
 
 const findDiff = (filepath1, filepath2) => {
   const file1 = readFileSync(path.resolve(process.cwd(), filepath1));
   const file2 = readFileSync(path.resolve(process.cwd(), filepath2));
-
   const parsedFile1 = parse(filepath1, file1);
   const parsedFile2 = parse(filepath2, file2);
 
@@ -18,25 +18,26 @@ const findDiff = (filepath1, filepath2) => {
   const keysOfFile2 = Object.keys(parsedFile2);
 
   const allSortedKeys = _.sortBy(_.union(keysOfFile1, keysOfFile2));
-
-  const result = allSortedKeys.reduce((acc, key) => {
-    if (parsedFile1.hasOwnProperty(key) && parsedFile2.hasOwnProperty(key)) {
+  if (allSortedKeys.length === 0) return '{}';
+  const differences = allSortedKeys.reduce((acc, key) => {
+    if (_.has(parsedFile1, key) && _.has(parsedFile2, key)) {
       if (parsedFile1[key] === parsedFile2[key]) {
-        acc[`  ${key}`] = parsedFile1[key];
+        acc.push(`    ${key}: ${parsedFile1[key]}`);
       } else {
-        acc[`- ${key}`] = parsedFile1[key];
-        acc[`+ ${key}`] = parsedFile2[key];
+        acc.push(`  - ${key}: ${parsedFile1[key]}`);
+        acc.push(`  + ${key}: ${parsedFile2[key]}`);
       }
-    } else if (parsedFile1.hasOwnProperty(key)) {
-      acc[`- ${key}`] = parsedFile1[key];
+    } else if (_.has(parsedFile1, key)) {
+      acc.push(`  - ${key}: ${parsedFile1[key]}`);
     } else {
-      acc[`+ ${key}`] = parsedFile2[key];
+      acc.push(`  + ${key}: ${parsedFile2[key]}`);
     }
     return acc;
-  }, {});
+  }, []);
 
-  console.log(JSON.stringify(result, null, 2).replace(/["]+/g, ''));
-  return JSON.stringify(JSON.stringify(result, null, 2).replace(/["]+/g, ''));
+  const result = '{\n'.concat(differences.join(',\n')).concat('\n}');
+  console.log(result);
+  return result;
 };
 
 export default findDiff;
