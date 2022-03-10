486027eba5aefb18b881be79b9bfff41391441c1
Tatiana-Popova <Tatiana-Popova@users.noreply.github.com>
2022-03-10 18:02:19 +0400

    refactor fileDiff.js

--- src/fileDiff.js
+++ src/fileDiff.js
@@ -2,37 +2,49 @@ import { readFileSync } from 'fs';
 import _ from 'lodash';
 import path from 'path';
 import parse from './parsers.js';
+import stylish from './formatters/stylish.js';
 
-const findDiff = (filepath1, filepath2) => {
-  const file1 = readFileSync(path.resolve(process.cwd(), filepath1));
-  const file2 = readFileSync(path.resolve(process.cwd(), filepath2));
-  const parsedFile1 = parse(filepath1, file1);
-  const parsedFile2 = parse(filepath2, file2);
-
-  const keysOfFile1 = Object.keys(parsedFile1);
-  const keysOfFile2 = Object.keys(parsedFile2);
-
-  const allSortedKeys = _.sortBy(_.union(keysOfFile1, keysOfFile2));
-  if (allSortedKeys.length === 0) return '{}';
+const createDifferencesObject = (object1, object2) => {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  const allSortedKeys = _.sortBy(_.union(object1Keys, object2Keys));
   const differences = allSortedKeys.reduce((acc, key) => {
-    if (_.has(parsedFile1, key) && _.has(parsedFile2, key)) {
-      if (parsedFile1[key] === parsedFile2[key]) {
-        acc.push(`    ${key}: ${parsedFile1[key]}`);
-      } else {
-        acc.push(`  - ${key}: ${parsedFile1[key]}`);
-        acc.push(`  + ${key}: ${parsedFile2[key]}`);
-      }
-    } else if (_.has(parsedFile1, key)) {
-      acc.push(`  - ${key}: ${parsedFile1[key]}`);
+    const doHaveBothObjectsAKey = _.has(object1, key) && _.has(object2, key);
+    if (!doHaveBothObjectsAKey) {
+      const [value, owner] = _.has(object1, key) ? [object1[key], 1] : [object2[key], 2];
+      acc[key] = {
+        toSimpleCopy: true,
+        key,
+        value,
+        owner,
+      };
     } else {
-      acc.push(`  + ${key}: ${parsedFile2[key]}`);
+      const AreBothValuesObject = _.isObject(object1[key]) && _.isObject(object2[key]);
+      const AreBothValuesArray = Array.isArray(object1[key]) && Array.isArray(object2[key]);
+      if (!AreBothValuesObject) {
+        acc[key] = { toSimpleCopy: true, key, value: [object1[key], object2[key]] };
+      } else if (AreBothValuesObject && !AreBothValuesArray) {
+        acc[key] = {
+          toSimpleCopy: false,
+          key,
+          children: createDifferencesObject(object1[key], object2[key]),
+        };
+      }
     }
     return acc;
-  }, []);
+  }, {});
+  return differences;
+};
 
-  const result = '{\n'.concat(differences.join(',\n')).concat('\n}');
-  console.log(result);
+const findDifferences = (filepath1, filepath2) => {
+  const file1 = readFileSync(path.resolve(process.cwd(), filepath1));
+  const file2 = readFileSync(path.resolve(process.cwd(), filepath2));
+
+  const parsedFile1 = parse(filepath1, file1);
+  const parsedFile2 = parse(filepath2, file2);
+  const differences = createDifferencesObject(parsedFile1, parsedFile2);
+  const result = stylish(differences);
   return result;
 };
 
-export default findDiff;
+export default findDifferences;
