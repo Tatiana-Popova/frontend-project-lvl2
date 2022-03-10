cd892e04514c114243a6897cbd7f05e3e79f69c4
Tatiana-Popova <Tatiana-Popova@users.noreply.github.com>
2022-03-10 18:01:22 +0400

    refactor

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 // import path from 'path';
 
-import findDiff from '../src/fileDiff.js';
+import findDifferences from '../src/fileDiff.js';
 
 let jsonPath1 = '';
 let jsonPath2 = '';
@@ -18,21 +18,19 @@ beforeAll(() => {
   const __dirname = dirname(__filename);
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-  jsonPath1 = getFixturePath('testFile1.json');
-  jsonPath2 = getFixturePath('testFile2.json');
+  jsonPath1 = getFixturePath('recursiveFile1.json');
+  jsonPath2 = getFixturePath('recursiveFile2.json');
+  ymlPath1 = getFixturePath('recursiveFile1.yml');
+  ymlPath2 = getFixturePath('recursiveFile2.yml');
+  const resultJson = getFixturePath('recursiveResult.txt');
 
-  ymlPath1 = getFixturePath('testFile1.yml');
-  ymlPath2 = getFixturePath('testFile2.yml');
-
-  const resultJson = getFixturePath('result.txt');
-
-  result = readFileSync(resultJson, 'utf8');
+  result = readFileSync(resultJson, 'utf-8');
 });
 
 test('findDifferenceJSON', () => {
-  expect(findDiff(jsonPath1, jsonPath2)).toEqual(result);
+  expect(findDifferences(jsonPath1, jsonPath2)).toEqual(result);
 });
 
 test('findDifferenceYAML', () => {
-  expect(findDiff(ymlPath1, ymlPath2)).toEqual(result);
+  expect(findDifferences(ymlPath1, ymlPath2)).toEqual(result);
 });
