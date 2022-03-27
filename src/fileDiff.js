cd6f31419f9ac5677019229a87e4d42968b3003f
Tatiana-Popova <Tatiana-Popova@users.noreply.github.com>
2022-03-27 19:31:46 +0400

    refactor

--- src/fileDiff.js
+++ src/fileDiff.js
@@ -8,69 +8,71 @@ const createDifferencesTree = (object1, object2) => {
   const object1Keys = Object.keys(object1);
   const object2Keys = Object.keys(object2);
   const allSortedKeys = _.sortBy(_.union(object1Keys, object2Keys));
+
   const differences = allSortedKeys.reduce((acc, key) => {
     const doHaveBothObjectsAKey = _.has(object1, key) && _.has(object2, key);
     if (!doHaveBothObjectsAKey) {
       const [value, owner] = _.has(object1, key) ? [object1[key], 1] : [object2[key], 2];
-      acc[key] = {
-        toSimpleCopy: true,
-        key,
-        value,
-        owner,
+      return {
+        ...acc,
+        [key]: {
+          toSimpleCopy: true,
+          key,
+          value,
+          owner,
+        },
       };
-      return acc;
     }
     const AreBothValuesObject = _.isObject(object1[key]) && _.isObject(object2[key]);
     const AreBothValuesArray = Array.isArray(object1[key]) && Array.isArray(object2[key]);
     if (!AreBothValuesObject) {
-      acc[key] = { toSimpleCopy: true, key, value: [object1[key], object2[key]] };
-      return acc;
+      return {
+        ...acc,
+        [key]: { toSimpleCopy: true, key, value: [object1[key], object2[key]] },
+      };
     }
     if (AreBothValuesObject && !AreBothValuesArray) {
-      acc[key] = {
-        toSimpleCopy: false,
-        key,
-        children: createDifferencesTree(object1[key], object2[key]),
+      return {
+        ...acc,
+        [key]: {
+          toSimpleCopy: false,
+          key,
+          children: createDifferencesTree(object1[key], object2[key]),
+        },
       };
-      return acc;
     }
-
-    // return acc;
+    return null;
   }, {});
+  //   return acc;
+  // }, {});
+
+  // const differences = allSortedKeys.reduce((acc, key) => {
+  //   const doHaveBothObjectsAKey = _.has(object1, key) && _.has(object2, key);
+  //   if (!doHaveBothObjectsAKey) {
+  //     const [value, owner] = _.has(object1, key) ? [object1[key], 1] : [object2[key], 2];
+  //     acc[key] = {
+  //       toSimpleCopy: true,
+  //       key,
+  //       value,
+  //       owner,
+  //     };
+  //   } else {
+  //     const AreBothValuesObject = _.isObject(object1[key]) && _.isObject(object2[key]);
+  //     const AreBothValuesArray = Array.isArray(object1[key]) && Array.isArray(object2[key]);
+  //     if (!AreBothValuesObject) {
+  //       acc[key] = { toSimpleCopy: true, key, value: [object1[key], object2[key]] };
+  //     } else if (AreBothValuesObject && !AreBothValuesArray) {
+  //       acc[key] = {
+  //         toSimpleCopy: false,
+  //         key,
+  //         children: createDifferencesTree(object1[key], object2[key]),
+  //       };
+  //     }
+  //   }
+  //   return acc;
+  // }, {});
   return differences;
 };
-
-// const createDifferencesTree = (object1, object2) => {
-//   const object1Keys = Object.keys(object1);
-//   const object2Keys = Object.keys(object2);
-//   const allSortedKeys = _.sortBy(_.union(object1Keys, object2Keys));
-//   const differences = allSortedKeys.reduce((acc, key) => {
-//     const doHaveBothObjectsAKey = _.has(object1, key) && _.has(object2, key);
-//     if (!doHaveBothObjectsAKey) {
-//       const [value, owner] = _.has(object1, key) ? [object1[key], 1] : [object2[key], 2];
-//       acc[key] = {
-//         toSimpleCopy: true,
-//         key,
-//         value,
-//         owner,
-//       };
-//     } else {
-//       const AreBothValuesObject = _.isObject(object1[key]) && _.isObject(object2[key]);
-//       const AreBothValuesArray = Array.isArray(object1[key]) && Array.isArray(object2[key]);
-//       if (!AreBothValuesObject) {
-//         acc[key] = { toSimpleCopy: true, key, value: [object1[key], object2[key]] };
-//       } else if (AreBothValuesObject && !AreBothValuesArray) {
-//         acc[key] = {
-//           toSimpleCopy: false,
-//           key,
-//           children: createDifferencesTree(object1[key], object2[key]),
-//         };
-//       }
-//     }
-//     return acc;
-//   }, {});
-//   return differences;
-// };
 
 const readFile = (filepath) => {
   const fullPath = path.resolve(process.cwd(), filepath);
